881c4cab780bacfdd2095760885605e6015c68ce
Clodiap <clodiapp@gmail.com>
2019-01-19 20:21:51 +0100

    added .rubocop.yml file to the lib folder

--- lib/mkdiruby.rb
+++ lib/mkdiruby.rb
@@ -2,11 +2,9 @@
 def check_if_user_gave_input
   abort("Relance la commande suivie du nom du dossier à créer") if ARGV.empty?
 end
-
 def get_folder_name
   return folder_name = ARGV.join("_")
 end
-
 def create_folder(folder_name)
   Dir.mkdir(folder_name)
 end
@@ -14,12 +12,12 @@ end
 # création et écriture du Gemfile dans un dossier
 def create_gemfile(folder_name)
   file = File.open("#{folder_name}/Gemfile", "a")
-  file.puts("source \"https://rubygems.org\"")
+  file.puts("source 'https://rubygems.org'")
   file.puts("ruby '2.5.1'")
-  file.puts("gem 'rubocop', '~> 0.57.2'")
-  file.puts("gem 'rspec'")
-  file.puts("gem 'pry'")
   file.puts("gem 'dotenv'")
+  file.puts("gem 'pry'")
+  file.puts("gem 'rspec'")
+  file.puts("gem 'rubocop', '~> 0.57.2'")
   file.close
 end
 
@@ -40,6 +38,7 @@ def create_env_file_gitignore(folder_name)
   file.puts(".env")
   file.close
 end
+
 # Création du dossier lib
 def create_folder_lib(folder_name)
   Dir.mkdir("#{folder_name}/lib")
@@ -65,7 +64,28 @@ def create_github_repo(folder_name)
     system("cd #{folder_name}\ngit push origin master")
 end
 
-
+# Création d'un fichier .rubocop.yml pour que rubocop soit moins strict
+def create_rubocop_file(folder_name)
+  file = File.open("#{folder_name}/lib/.rubocop.yml", "a")
+  file.puts("inherit_from:\n- http://relaxed.ruby.style/rubocop.yml\n
+AllCops:
+DisplayStyleGuide: true
+DisplayCopNames: true\n
+Exclude:
+- 'db/schema.rb'
+- 'vendor/**/*'
+- 'config/environments/*.rb'
+- 'bin/*'\n
+Rails:
+Enabled: True\n
+Metrics/BlockLength:
+Exclude:
+- '../spec/**/*.rb'
+- '../spec/spec_helper.rb'
+- 'Guardfile'
+- 'vendor/bundle')")
+  file.close
+end
 
 def perform
   check_if_user_gave_input
@@ -80,6 +100,7 @@ def perform
   create_readme_ruby_program(folder_name)
   bundle_install(folder_name)
   create_github_repo(folder_name)
+  create_rubocop_file(folder_name)
 end
 
 perform
